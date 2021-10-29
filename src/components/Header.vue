2b28db9305b3b4e3eb7cdf7f65144ed8a2520d38
Kévin <kevin.joya@outlook.com>
2021-10-29 10:37:52 +0200

    Created compo Header

---
+++ src/components/Header.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="header">
+    <div class="header__left">
+      <div class="header--logo">
+        <img src="../assets/logo.svg" alt="logo">
+      </div>
+      <div>
+        <navbar-left></navbar-left>
+      </div>
+    </div>
+    <div class="header__right">
+      <div>
+        <navbar-right></navbar-right>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import NavbarLeft from './Navbar/NavbarLeft.vue';
+import NavbarRight from './Navbar/NavbarRight.vue';
+
+export default {
+  components: { NavbarLeft, NavbarRight },
+  name: 'Header',
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang='scss' scoped>
+  .header {
+    display: flex;
+    justify-content: space-around;
+      &__left {
+        display: flex;
+        .header--logo {
+          margin-right: 45px;
+        }
+      }
+  }
+</style>
